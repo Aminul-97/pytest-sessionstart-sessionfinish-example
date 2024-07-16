dfbcfcb27537084c26286118a330a345c4287ab0
ericsalesdeandrade <sdaeric19@gmail.com>
2024-07-16 09:24:54 +0100

    Refactor

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,19 +1,16 @@
 import pytest
-from src.api_manager import Manage_api
+from src.leap_year import LeapYear
+
 
 @pytest.hookimpl()
 def pytest_sessionstart(session):
-    try:
-        api_mgr = Manage_api("https://digidates.de/api/v1/checkdate?date=2023-01-01")
-        if api_mgr.check_connection() == 200:
-             print("API connection successful!")
-        else:
-            raise Exception("Invalid link.")
-    except:
-        raise Exception("Failed to connect!")
-    print("==== Entering the tests ====")
+    leap_year = LeapYear("https://digidates.de/api/v1/leapyear")
+    if leap_year.check_connection() == 200:
+        print("API connection successful!")
+    else:
+        pytest.exit("API Connection Unsuccessful!")
+
 
 @pytest.hookimpl()
 def pytest_sessionfinish(session):
     print("\nTest session finished!")
-    
